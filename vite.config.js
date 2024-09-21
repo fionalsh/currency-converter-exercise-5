3f3bd9b64e3f9dded7cbc95247a096c0085889e1
fionalsh <fionals4@gmail.com>
2024-09-21 10:43:27 +0800

    update base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/currncy-converter-exercise-5/'
+  base: '/currency-converter-exercise-5/'
 })
